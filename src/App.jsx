490363f5e4391b01a0c17ffc85b309274191d475
Fikar <zulfikarkhasb@gmail.com>
2025-08-12 19:52:17 +0700

    WOILAH KNP NI

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Halo Fikar</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          Itungan nya {count}
+          Itungan {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
