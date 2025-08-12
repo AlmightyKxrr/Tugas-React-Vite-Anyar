1212b022c4fc59582104d08563038e4c8093b1b1
Fikar <zulfikarkhasb@gmail.com>
2025-08-12 19:43:14 +0700

    Woilah berubah

--- src/App.jsx
+++ src/App.jsx
@@ -19,14 +19,14 @@ function App() {
       <h1>Halo Fikar</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          Itungan nya {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Klik di Logo Vite dan React buat pelajari lebih lanjut cuyyyy
       </p>
     </>
   )
